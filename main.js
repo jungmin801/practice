68fc0611d71ad7b6f365c08edf9d8e39cf9906ac
Jungmin Moon <jm.moon0801@gmail.com>
2023-04-17 23:34:01 +0900

    second commit

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 // const callAPI = async() =>{
-//     let url = new URL(`https://api.edamam.com/search?q=vegan&app_id=e3cf5ffc&app_key=d9c140d574c8e323e87a00a286665ad4&from=0&to=9`)
+//     let url = new URL(`https://api.edamam.com/search?q=vegan&app_id=e3cf5ffc&app_key=d9c140d574c8e323e87a00a286665ad4&from=0&to=15`)
 //     let header = new Headers({'x-api-key':'d9c140d574c8e323e87a00a286665ad4'})
 //     let response = await fetch(url,{headers:header})
 //     let data = await response.json()
 
-//     console.log(data)
+
+//     console.log("response는", response)
+//     console.log("데이터는", data)
 // }
+    
 
 // callAPI()
+
+
