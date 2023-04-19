952dc0218fee5a4fe91bee8499e7b1e75d4f8bdc
Jungmin Moon <jm.moon0801@gmail.com>
2023-04-19 20:55:31 +0900

    5th commit

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ let recipesList = [];
 let categoryButton = document.querySelectorAll(".category-area button")
 let searchButton = document.getElementById("search-button")
 let url;
+let totalCount = 0;
+let currentPage = 1;
 
 
 //버튼 클릭
@@ -10,20 +12,51 @@ categoryButton.forEach(item=>item.addEventListener("click",(event)=>ByCategory(e
 
 
 //레시피 가져오기
-
 const getRecipes = async() => {
+  try{
+
   let header = new Headers({'x-api-key':'d9c140d574c8e323e87a00a286665ad4'});
+
+  url.searchParams.set("from", currentPage)
+  url.searchParams.set("to", currentPage+21)
+  console.log(url)
+
+
   let response = await fetch(url,{headers:header});
   let data = await response.json();
 
-  recipes = data.hits;
-  recipesList = [];
+  if(response.status == 200){
+    if(data.count == 0 ){
+      throw new Error("No matches for your search.")
+    }
 
-  recipes.forEach((item) => {
-      recipesList.push(item.recipe);
+    recipes = data.hits;
+    totalCount = data.count;
+    recipesList = [];
+
+    recipes.forEach((item) => {
+        recipesList.push(item.recipe);
     });
 
-    render()
+  render();
+  pagination();
+
+  } else {
+    throw new Error(error.message);
+  }
+  } catch(error){
+    errorRender(error.message);
+  }
+}
+
+
+
+//레시피 불러오기(메인화면)
+const getNewestRecipes = async() =>{
+    url = new URL(`https://api.edamam.com/search?q=vegan&app_id=e3cf5ffc&app_key=d9c140d574c8e323e87a00a286665ad4&from=1&to=22`);
+    console.log(url);
+    
+    getRecipes();
 }
 
 
@@ -31,30 +64,19 @@ const getRecipes = async() => {
 const searchByKeyword = async() => {
 
   let Keyword = document.getElementById("search-input").value
-  url = new URL(`https://api.edamam.com/search?q=${Keyword}&app_id=e3cf5ffc&app_key=d9c140d574c8e323e87a00a286665ad4&from=0&to=21`);
+  url = new URL(`https://api.edamam.com/search?q=vegan+${Keyword}&app_id=e3cf5ffc&app_key=d9c140d574c8e323e87a00a286665ad4&from=1&to=22`);
 
   getRecipes();
   
 }
 
-
-
 // 카테고리별 보여주기
 const ByCategory = async(event) => {
-    event.target.textContent.toLowerCase();
-    url = new URL(`https://api.edamam.com/search?q=vegan&app_id=e3cf5ffc&app_key=d9c140d574c8e323e87a00a286665ad4&from=0&to=21&dishType=${categorys}`);
- 
+  let category = event.target.textContent.toLowerCase();
+  url = new URL(`https://api.edamam.com/search?q=vegan&app_id=e3cf5ffc&app_key=d9c140d574c8e323e87a00a286665ad4&from=1&to=22&dishType=${category}`);
+
   getRecipes();
 }
-
-
-//레시피 불러오기(메인화면)
-const getNewestRecipes = async() =>{
-    url = new URL(`https://api.edamam.com/search?q=vegan&app_id=e3cf5ffc&app_key=d9c140d574c8e323e87a00a286665ad4&from=0&to=21`);
-    getRecipes();
-}
-
-
 
 
     
@@ -65,10 +87,16 @@ const render = () => {
     recipesHTML = recipesList.map((item) => {
         return `<div class="col">
         <div class="card h-100 shadow-sm">
-          <img src="${item.image}" class="card-img-top">
+          <img class="card-img-top" src="${
+            item.image == null 
+          ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
+          : item.image}">
           <div class="card-body card-body-area">
             <div class="card-title-area">
-              <h5 class="card-title" style="font-weight: bold">${item.label}</h5>
+              <h5 class="card-title" style="font-weight: bold">${
+                item.label == null
+              ? "Unknown"
+              : item.label}</h5>
             </div>
             <div class="card-text card-text-area">
                 <div class="card-text-info">
@@ -78,7 +106,11 @@ const render = () => {
                         : item.totalTime+"min"
                     }</span>
                     <span>
-                    <i class="fa-solid fa-fire"></i> ${Math.ceil(item.calories/item.yield)}Kcal/serving</span>
+                    <i class="fa-solid fa-fire"></i> ${
+                      Math.ceil(item.calories/item.yield) < 0
+                      ? "Unknown"
+                      : Math.ceil(item.calories/item.yield)+"Kcal/serving"
+                      }</span>
                 </div>
             </div>
           </div>
@@ -89,6 +121,41 @@ const render = () => {
     document.getElementById("recipes-board").innerHTML = recipesHTML;
 }
 
+//에러 나타내기
+
+const errorRender = (message) => {
+  let errorHTML = '';
+  errorHTML = `<div class="alert alert-danger text-center error-area w-100" role="alert"> ${message} </div>`
+  document.getElementById("recipes-board").innerHTML = errorHTML;
+}
+
+// 페이지네이션
+// totalCount: 총 데이터의 개수 -> 7000개
+// totalPage : 총 페이지 수 -> 
+// currentPage : 현재 페이지 -> 1
+// pageCount: 화면에 나타날 페이지 개 -> 10개
+// limit: 한 페이지 당 나타낼 데이터의 개수 -> 21개
+// pageGroup : 페이지 개수 10개 = 페이지 그룹 1개
+
+const pagination = () => {
+    let paginationHTML="";
+
+    let pageGroup = Math.ceil(currentPage/10);
+    let last = pageGroup*10
+    let first = last - 9
+
+    for(let i=first;i<=last;i++){
+      paginationHTML += `<li class="page-item ${currentPage==i? "active":""}"><a class="page-link" href="#" onclick="moveToPage(${i})">${i}</a></li>`
+    }
+
+    document.querySelector(".pagination").innerHTML = paginationHTML; 
+}
+
+const moveToPage = (pageNum) => {
+  currentPage = pageNum;
+  console.log(currentPage)
+  getRecipes()
+}
 
 searchByKeyword();
 ByCategory();
